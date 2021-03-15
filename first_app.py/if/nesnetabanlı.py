d3bba4c4a79f42bcc227a11f79e8638a228011ca
sindaakyil <73520969+sindaakyil@users.noreply.github.com>
2021-03-15 22:45:58 +0300

    updates

---
+++ first_app.py/if/nesnetabanlı.py
@@ -0,0 +1,14 @@
+#object oriented programing(OOP) NESNE TABANLI PROGRAMLAMA
+#CLASS SINIF= class bizim için  kütüphane içerisinde daha önce oluşturulmuş belli başlı bazı özellikler ve belli başlı bazı method lar class içerisine aktarılmış class tann sonra ya method yada ettribute tanıtmamız gerekiyor
+#İNSTANCE (OBJE)
+
+lst=[1,2,3] #lst classından oluşturulmuş
+result = type(lst)
+print(result)
+
+#class  class'a verilen isim büyük harfle başlar
+
+class Person  
+#object, instance
+#class attributes
+constructor
---
+++ first_app.py/if/orn.py
@@ -0,0 +1,14 @@
+#girilen bir sayının asal olup olmadığını bulun
+a=int(input('sayıyı giriniz'))
+b=int(input('sayıyı giriniz'))
+tam=a/b
+c= 
+print(c)
+if (a% b== 0):
+    int=int(tam)
+    print(f'tam bölünür :{tam}')
+else:
+    tam=float(tam)
+    print(f':{tam}')
+    tam=int(tam)
+    print(tam)
